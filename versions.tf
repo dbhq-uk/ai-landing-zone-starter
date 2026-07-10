2729feda848367d7a9957e71579c83f11ae0df1b
Daniel Grimes <dan@dbhq.uk>
2026-07-10 15:18:14 +0000

    feat: wire azurerm remote backend and module providers

--- versions.tf
+++ versions.tf
@@ -1,10 +1,33 @@
 terraform {
-  required_version = ">= 1.9"
+  required_version = ">= 1.9, < 2.0"
+
+  backend "azurerm" {
+    resource_group_name  = "rg-dbhq-housekeeping"
+    storage_account_name = "dbhqtfstateuks01"
+    container_name       = "tfstate"
+    key                  = "ai-landing-zone-starter.tfstate"
+  }
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.4"
+    }
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.12"
     }
   }
 }
